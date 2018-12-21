7de2e8c2a0845c2e7bc5466a32700471e8923e1b
Romy Haacke <romy@haacke.tv>
2018-12-21 21:53:01 +0100

    Return constants to webserver

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL= 'http://192.168.2.104:3000/api/';//'https://star-chat-server.herokuapp.com/api/';
-export const SOCKET_URL = 'http://192.168.2.104:3000';
+export const API_URL= 'https://star-chat-server.herokuapp.com/api/';
+export const SOCKET_URL = 'https://star-chat-server.herokuapp.com/';
